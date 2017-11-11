01f4c02086f295c3f7e196c5450f9a7e69e86c7f
Bartosz Romanowski <bartosz.romanowski2@gmail.com>
2017-11-11 14:51:35 +0100

    Schema changes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-/*CREATE TABLE users (
+CREATE TABLE users (
   id serial NOT NULL,
   name varchar(45) NOT NULL,
   PRIMARY KEY(id)
@@ -55,13 +55,8 @@ CREATE TABLE debt
 
 CREATE INDEX fk_debt_product1 ON debt (product_id);
 CREATE INDEX fk_debt_user1 ON debt (creditor_id);
-CREATE INDEX fk_debt_user2 ON debt (debtor_id);*/
+CREATE INDEX fk_debt_user2 ON debt (debtor_id);
 
-/*
 INSERT INTO users (id, name) VALUES (1, 'Bartek'), (2, 'Aga'), (3, 'Rafał');
 INSERT INTO user_group (id, name) VALUES (1, 'Wszyscy'), (2, 'Rafał i Aga'), (3, 'Bartek');
 INSERT INTO user_user_group (user_id, user_group_id) VALUES (1, 1), (1, 3), (2, 1), (2, 2), (3, 1), (3, 2);
-*/
-/*
-ALTER TABLE debt RENAME cretiondate TO creation_date;
-*/
